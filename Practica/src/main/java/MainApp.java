b11e9261898aed317174dd56bc505a404427e88b
Santiago Gomez <santiago.gomez@fit-bank.com>
2019-04-14 14:41:14 -0500

    asdasd

---
+++ Practica/src/main/java/MainApp.java
@@ -0,0 +1,12 @@
+
+public class MainApp {
+
+	public String sayHello() {
+	      return "Hello Gradle";
+	   }
+
+	   public static void main(String[] args) {
+	      MainApp app = new MainApp();
+	      System.out.println(app.sayHello());
+	   }
+}
